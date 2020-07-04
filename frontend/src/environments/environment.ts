0c8e09d69ba0c05bfe7c039afe862cf019971f87
Baher Wael <bwy@crossworkers.com>
2020-07-05 00:46:58 +0200

    environment setup

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'bahers-coffee-shop.us', // the auth0 domain prefix
+    audience: 'api', // the audience set for the auth0 app
+    clientId: 'ax0ax8oUaSIHs6AmwIOZ4eLqcmJRjlz7', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
